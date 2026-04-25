7e27122eaa4ef436b889c662ae7539fcc9c7449e
Sanjay <sanjay7051b@gmail.com>
2026-04-25 15:59:12 +0530

    fix api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-// 🔥 ADD THIS INTERCEPTOR
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
